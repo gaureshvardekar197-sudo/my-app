120fdb6c938528f55a2a464180e0a29ade46e3c5
gaureshvardekar <gaureshvardekar197@gmail.com>
2025-11-10 20:06:36 +0530

    doc:add space

--- src/component/Increment.js
+++ src/component/Increment.js
@@ -23,7 +23,7 @@ function Counter() {
         <div>
             <h1>count:{count}</h1>
             <h1>name:{name}</h1>
-            <h1>address:{pincode}&nbsp;{cityname}</h1>
+            <h1>address:{pincode}&nbsp; {cityname}</h1>
             <h1>age:{age}</h1>
             <button onClick={increment} id="Inc">Increment</button>
             <button onClick={decrement} id="Dec">Decrement</button>
